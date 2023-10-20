e42e1be203d313136fd134b9f3107a2c2d56886e
Galaticos402 <64137012+Galaticos402@users.noreply.github.com>
2023-10-20 23:46:43 +0700

    Update post.api.ts

--- src/api/post/post.api.ts
+++ src/api/post/post.api.ts
@@ -4,8 +4,8 @@ import { CreatePostParams, GetPostResult } from "./post.model";
 
 export const PostAPI = {
   getAllPost: async () : Promise<AxiosResponse<GetPostResult[]>> => {
-    const res = httpClient.get<GetPostResult[]>(`/Post`);
-    return res;
+    const res = await httpClient.get<GetPostResult[]>(`/Post`);
+    return res.data;
   },
   createPost: async (params: CreatePostParams) => {
     const res = await httpClient.post<any>(`/Post`, params);
